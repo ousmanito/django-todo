606f95652aca73926b17310f1ff02333d746ca29
ousmanito <ousmane.209@gmail.com>
2022-03-27 20:03:54 +0200

    css

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 
