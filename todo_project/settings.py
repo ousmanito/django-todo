bf5c891589a5d0e635dc0637b498dd30dea500c6
Ousmane <ousmane.209@gmail.com>
2023-10-16 16:44:19 +0200

    fix: fix STATIC_URL in settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_URL = 'static'
+STATIC_URL = 'static/'
 
 
 LOGIN_URL = 'login/'
